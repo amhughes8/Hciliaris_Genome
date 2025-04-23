6f1e9f1988a9bb13c1caed4539b83a30857004d3
Annabel Hughes <110560869+amhughes8@users.noreply.github.com>
2025-04-23 17:15:36 -0400

    Create busco_flye_hq_scaff.sh

---
+++ SLURM_batch_jobs/busco_flye_hq_scaff.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J busco_flye_hq_scaff                        # Job name
+#SBATCH -p short                            # Partition
+#SBATCH -N 1                                # Number of nodes
+#SBATCH -n 25                                # Number of tasks/threads
+#SBATCH -o output_%j.txt                    # Standard output file
+#SBATCH -e error_%j.txt                     # Standard error file
+#SBATCH --mail-user=hughes.annab@northeastern.edu  # Email
+#SBATCH --mail-type=END                     # Email notification at job completion
+#SBATCH --time=48:00:00                     # Maximum run time
+
+
+# Your program/command here
+module load anaconda3/2022.05
+source activate /work/gatins/hci_genome/busco
+busco -i /work/gatins/hci_genome/processing/assembly_Flye_hq_scaff/assembly.fasta --mode genome --lineage_dataset actinopterygii_odb12 --cpu 25 --out flye_hq_scaff_busco
