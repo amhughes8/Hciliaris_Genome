686256cb28993e40ac5ce68942c4e4d62e5816ee
Annabel Hughes <110560869+amhughes8@users.noreply.github.com>
2025-04-23 17:20:26 -0400

    Create medaka_inference.sh

---
+++ SLURM_batch_jobs/medaka_inference.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J medaka_inference                        # Job name
+#SBATCH -p short                            # Partition
+#SBATCH -N 1                                # Number of nodes
+#SBATCH -n 2                                # Number of tasks/threads
+#SBATCH -o output_%j.txt                    # Standard output file
+#SBATCH -e error_%j.txt                     # Standard error file
+#SBATCH --mail-user=hughes.annab@northeastern.edu  # Email
+#SBATCH --mail-type=END                     # Email notification at job completion
+#SBATCH --time=48:00:00                     # Maximum run time
+
+
+# Your program/command here
+module load anaconda3/2022.05
+source activate medaka
+medaka inference /work/gatins/hci_genome/processing/medaka/medaka_align.bam.bam /work/gatins/hci_genome/processing/medaka/results --model r1041_e82_400bps_sup_v5.0.0 --threads 2
