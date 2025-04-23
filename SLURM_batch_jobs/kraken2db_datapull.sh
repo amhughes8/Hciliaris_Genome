783634aa38f60c60efa73cf50a5d3b0f75bd5633
Annabel Hughes <110560869+amhughes8@users.noreply.github.com>
2025-04-23 17:17:28 -0400

    Create kraken2db_datapull.sh

---
+++ SLURM_batch_jobs/kraken2db_datapull.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -J medaka_align                        # Job name
+#SBATCH -p short                            # Partition
+#SBATCH -N 1                                # Number of nodes
+#SBATCH -n 32                                # Number of tasks/threads
+#SBATCH --mem=200G                          # Memory
+#SBATCH -o output_%j.txt                    # Standard output file
+#SBATCH -e error_%j.txt                     # Standard error file
+#SBATCH --mail-user=hughes.annab@northeastern.edu  # Email
+#SBATCH --mail-type=END                     # Email notification at job completion
+#SBATCH --time=48:00:00                     # Maximum run time
+
+
+# Your program/command here
+module load python/3.8.1
+module load anaconda3/2022.05
+source activate /work/gatins/hci_genome/env
+cd /work/gatins/hci_genome/processing/kraken2_builtpython
+python /work/gatins/hci_genome/kraken2/download_domain.py --domain archaea --complete True --ext dna
+python /work/gatins/hci_genome/kraken2/download_domain.py --domain viral --complete True --ext dna
+python /work/gatins/hci_genome/kraken2/download_domain.py --domain plasmid --complete True --ext dna
+python /work/gatins/hci_genome/kraken2/download_domain.py --domain vertebrate_mammalian --complete True --ext dna --human True
