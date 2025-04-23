caa887c2a4736b127902f0d38ffc0cba9b331ac4
Annabel Hughes <110560869+amhughes8@users.noreply.github.com>
2025-04-23 17:14:27 -0400

    Create assembly_flye_hq_scaff.sh

---
+++ SLURM_batch_jobs/assembly_flye_hq_scaff.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH -J assembly_flye_hq_scaff                        # Job name
+#SBATCH -p short                            # Partition
+#SBATCH -N 1                                # Number of nodes
+#SBATCH -n 32                                # Number of tasks/threads
+#SBATCH --mem=500G                          # Memory
+#SBATCH -o output_%j.txt                    # Standard output file
+#SBATCH -e error_%j.txt                     # Standard error file
+#SBATCH --mail-user=hughes.annab@northeastern.edu  # Email
+#SBATCH --mail-type=BEGIN,END                     # Email notification at job start and completion
+#SBATCH --time=48:00:00                     # Maximum run time
+
+
+# Your program/command here
+module load anaconda3/2022.05 discovery
+source activate /work/gatins/hci_genome/env
+flye --nano-hq /work/gatins/hci_genome/processing/hci_filtered_2.5kQ5.fastq --threads 32 --scaffold --out-dir /work/gatins/hci_genome/processing/assembly_Flye_hq_scaff
