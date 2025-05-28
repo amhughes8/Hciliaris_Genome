0772eb5adf0dd4d8cd864e9d316e7cdbdb732ec2
Annabel Hughes <110560869+amhughes8@users.noreply.github.com>
2025-05-28 14:13:39 -0400

    Create busco_hifiasm_nomito.sh

---
+++ SLURM_batch_jobs/busco_hifiasm_nomito.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -J busco_hifiasm_nomito                        # Job name
+#SBATCH -p short                            # Partition
+#SBATCH -N 1                                # Number of nodes
+#SBATCH -n 25                                # Number of tasks/threads
+#SBATCH -o output_%j.txt                    # Standard output file
+#SBATCH -e error_%j.txt                     # Standard error file
+#SBATCH --mail-user=hughes.annab@northeastern.edu  # Email
+#SBATCH --mail-type=END                     # Email notification at job completion
+#SBATCH --time=48:00:00                     # Maximum run time
+
+
+# Your program/command here
+module load anaconda3/2022.05
+source activate /work/gatins/hci_genome/busco
+busco -i /work/gatins/hci_genome/processing/mtdna/removal/hifiasm_nomito/assembly_hifiasm_no_mito.fa --mode genome --lineage_dataset actinopterygii_odb12 --cpu 25 --out hifiasm_nomito_busco
